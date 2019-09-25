656593107c5a164c1be3648f300853cdc0c6c31f
Joy Best <fasterthanmomentum@gmail.com>
2019-09-24 22:12:18 -0400

    up to p 674

--- JoySwiftControllers9242019/ViewController.swift
+++ JoySwiftControllers9242019/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var shareButtonTapped: UIButton!
+    
+    @IBOutlet weak var safariButtonTapped: UIButton!
+    
+    @IBOutlet weak var photoButtonTapped: UIButton!
+    
+    @IBOutlet weak var emailButtonTapped: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        
+        guard let image = imageView.image else { return }
+        
+        let activityController =
+            UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityController.popoverPresentationController?.sourceView = sender
+        
+        present(activityController, animated: true,
+                completion: nil)
+        
+        
+    }
+  
+    
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+        
+        if let url = URL(string: "http://www.apple.com") {
+        let safariViewController = SFSafariViewController(url: url)
+        present(safariViewController, animated: true,
+                completion: nil)
+    }
+    }
+  
+    
+    @IBAction func photoButtonTapped(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Choose Image Source", message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: {action in print("User selected Camera action")})
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: {action in print("User selected Photo Library action")})
+        
+        
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibraryAction)
+        alertController.popoverPresentationController?.sourceView = sender
+        
+        present(alertController, animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 
 }
 
